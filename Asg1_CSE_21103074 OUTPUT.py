67e6c201208f2012dc3ddcab5b68bee339444589
peeyushkk <97311162+peeyushkk@users.noreply.github.com>
2022-01-14 17:00:01 +0530

    Add files via upload

---
+++ Asg1_CSE_21103074 OUTPUT.py
@@ -0,0 +1,36 @@
+First Question
+Enter first number 4
+Enter second number 5
+Enter third number 6
+The average of three numbers is :
+5.0
+
+
+Second Question
+Enter your gross income 100000
+Enter your number of dependents 5
+Your income tax is :
+17399.0
+
+
+Third Question
+Enter your SID 21103074
+Enter your name Peeyush Kaushik 
+Enter your Gender M
+Enter your course name CSE
+Enter your CGPA 9
+[' 21103074', ' Peeyush Kaushik ', 'M', ' CSE', 9.0]
+
+Fourth Question
+Enter marks of students 97
+Enter marks of students 97
+Enter marks of students 96
+Enter marks of students 95
+Enter marks of students 94
+[' 94', ' 95', ' 96', ' 97', ' 97']
+
+Fifth Question
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
