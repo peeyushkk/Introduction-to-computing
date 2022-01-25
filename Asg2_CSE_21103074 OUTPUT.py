40eec25faddcac0849f1f73abac8077caa2d3dc1
peeyushkk <97311162+peeyushkk@users.noreply.github.com>
2022-01-26 00:08:48 +0530

    Add files via upload

---
+++ Asg2_CSE_21103074 OUTPUT.py
@@ -0,0 +1,42 @@
+First Question
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+Second Question
+ENTER YOUR NAME Peeyush Kaushik
+ENTER YOUR SID 21103074
+ENTER YOUR DEPARTMENT CSE
+ENTER YOUR CGPA 9.8
+Hey, Peeyush Kaushik Here!
+My SID is 21103074
+I am from  CSE and my CGPA is 9.800000
+
+
+Third Question
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Fourth Question
+Enter first number: 4
+Enter second number: 5
+Enter third number: 6
+THE LARGEST NUMBER IS  6
+
+
+Fifth Question
+ENTER A STRING Peeyush
+No
+
+
+Sixth Question
+ENTER FIRST SIDE OF TRIANGLE 4
+ENTER SECOND SIDE OF TRIANGLE 5
+ENTER THIRD SIDE OF TRIANGLE 6
+Yes
