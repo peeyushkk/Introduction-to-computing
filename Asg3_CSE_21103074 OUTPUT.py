ef4be7d361f4a094208a7c509083a11bba3b4207
peeyushkk <97311162+peeyushkk@users.noreply.github.com>
2022-02-14 23:51:30 +0530

    Add files via upload

---
+++ Asg3_CSE_21103074 OUTPUT.py
@@ -0,0 +1,78 @@
+
+QUESTION 1
+ENTER ANY STRING: PEEYUSH
+{'P': 1, 'E': 1, 'E': 1, 'Y': 1, 'U': 1, 'S': 1, 'H': 1}
+
+
+QUESTION 2
+ENTER THE DAY: 14
+ENTER THE MONTH OF THE YEAR: 01
+ENTER THE YEAR: 2003
+15 / 1 / 2003
+
+
+QUESTION 3
+Enter elements of a list separated by space 3 9 10
+list:  3 9 10
+[(3, 9), (9, 81), (10, 100)]
+
+
+QUESTION 4
+Give a number between 4 and 10 including them-9
+Performance=Excellent
+Letter Grade=A
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 21103074
+Enter Name: PEEYUSH
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103075
+Enter Name: DEEPANK
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103076
+Enter Name: SHREYANSH
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103077
+Enter Name: MILAN
+Do you want to enter another student entry(Y or N):N
+<a> {21103074: 'PEEYUSH', 21103075: 'DEEPANK', 21103076: 'SHREYANSH', 21103077: 'MILAN'}
+<b> {21103077: 'MILAN', 21103074: 'PEEYUSH', 21103075: 'DEEPANK', 21103076: 'SHREYANSH'}
+<c> {21103074: 'PEEYUSH', 21103075: 'DEEPANK', 21103076: 'SHREYANSH', 21103077: 'MILAN'}
+Search Name with SID: 21103075
+<d> DEEPANK
+
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 10
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 8.8
+
+
+
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
