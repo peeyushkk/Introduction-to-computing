2f49c007df4ee0ebe4727552533a514630aa3118
peeyushkk <97311162+peeyushkk@users.noreply.github.com>
2022-02-27 16:44:50 +0530

    Add files via upload

---
+++ Asg4_CSE_21103074 OUTPUT.py
@@ -0,0 +1,73 @@
+QUESTION 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+QUESTION 2
+Using iteration-
+Give the no. of rows-7
+      1  
+
+     1  1  
+
+    1  2  1  
+
+   1  3  3  1  
+
+  1  4  6  4  1  
+
+ 1  5  10  10  5  1  
+
+1  6  15  20  15  6  1  
+
+
+Using recursion-
+Please give no. of rows-7
+              1 
+
+             1 1 
+
+            1 2 1 
+
+           1 3 3 1 
+
+          1 4 6 4 1 
+
+         1 5 10 10 5 1 
+
+        1 6 15 20 15 6 1 
+
+
+QUESTION 3
+Enter 2 numbers: 10 3
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [7, 5, 6, 8, 6, 9, 7]
+<d> Set: {5, 6, 7, 8, 9}
+<e> Immutable set: frozenset({5, 6, 7, 8, 9})
+<f> Hash value of the max value from the above set: 9
+
+
+QUESTION 4 
+Object created
+The name of the student is Peeyush and SID is 21103074.
+Object destroyed
+
+
+QUESTION 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+
+QUESTION 6
+Enter the word: deal
+Enter a new meaningful word using the exact same letters to test your friendship: lead
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
